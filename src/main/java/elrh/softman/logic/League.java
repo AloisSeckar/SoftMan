5cf138f94298158e32336f0c73dde67a53e4f587
Alois Sečkár <ellrohir@seznam.cz>
2020-09-26 15:17:13 +0200

    Update League.java
    enhanced match scheduling (switching home/visitor stance each round) + fixtures preview

--- src/main/java/elrh/softman/logic/League.java
+++ src/main/java/elrh/softman/logic/League.java
@@ -2,48 +2,55 @@ package elrh.softman.logic;
 
 import elrh.softman.db.GameDBManager;
 import java.util.ArrayList;
-import java.util.Arrays;
+import lombok.Getter;
 
 public class League {
     
     private ArrayList<Team> teams = new ArrayList<>();
+    
+    @Getter
+    private int round = 1;
     
     public League(ArrayList<Team> teams) {
         this.teams = teams;
     }
     
     public void playLeague() {
-        for (int i = 1; i <= 9; i++) {
-           
+        for (int i = 1; i <= 36; i++) {
+            previewCurrentRound();
             playRound();
-            
-            shiftTeams();
         }
     }
     
-    ////////////////////////////////////////////////////////////////////////////
-    private void playRound() {
-        Match match = new Match(teams.get(0), teams.get(9));
-        match.simulate();
-        GameDBManager.getInstance().saveMatch(match);
-
-        match = new Match(teams.get(1), teams.get(8));
-        match.simulate();
-        GameDBManager.getInstance().saveMatch(match);
-
-        match = new Match(teams.get(2), teams.get(7));
-        match.simulate();
-        GameDBManager.getInstance().saveMatch(match);
-
-        match = new Match(teams.get(3), teams.get(6));
-        match.simulate();
-        GameDBManager.getInstance().saveMatch(match);
-
-        match = new Match(teams.get(4), teams.get(5));
-        match.simulate();
-        GameDBManager.getInstance().saveMatch(match);
+    public void playRound() {
+        Match match;
+        for (int i = 0; i < 5; i ++) {
+            if (round % 2 == 0) {
+                match = new Match(teams.get(i), teams.get(9 - i));
+            } else {
+                match = new Match(teams.get(9 - i), teams.get(i));
+            }
+            match.simulate();
+            GameDBManager.getInstance().saveMatch(match);
+        }
+        
+        shiftTeams();
+        round++;
+    }
+    
+    public void previewCurrentRound() {
+        System.out.println("LEAGUE ROUND " + round);
+        for (int i = 0; i < 5; i ++) {
+            if (round % 2 == 0) {
+                System.out.println(teams.get(i).getName() + " @ " + teams.get(9 - i).getName());
+            } else {
+                System.out.println(teams.get(9 - i).getName() + " @ " + teams.get(i).getName());
+            }
+        }
+        System.out.println("-----------------------");
     }
 
+    ////////////////////////////////////////////////////////////////////////////
     private void shiftTeams() {
         ArrayList<Team> shiftedTeams = new ArrayList<>();
         shiftedTeams.add(teams.get(0));
