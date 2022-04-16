ab46641d0ea7e006342f39414a824d3b466fa130
Alois Sečkár <ellrohir@seznam.cz>
2022-04-16 12:36:07 +0200

    player stats gauges
    featuring "Medusa" project

--- src/main/java/elrh/softman/gui/tile/PlayerInfoTile.java
+++ src/main/java/elrh/softman/gui/tile/PlayerInfoTile.java
@@ -1,16 +1,27 @@
 package elrh.softman.gui.tile;
 
 import elrh.softman.db.orm.PlayerInfo;
+import eu.hansolo.medusa.Gauge;
+import eu.hansolo.medusa.GaugeBuilder;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class PlayerInfoTile extends VBox {
 
     private final Label nameLabel;
     private final Label ageLabel;
+
+    private final Gauge overallGauge;
+    private final Gauge battingGauge;
+    private final Gauge pitchingGauge;
+    private final Gauge fieldingGauge;
+    private final Gauge physicalGauge;
 
     public PlayerInfoTile() {
 
@@ -35,12 +46,69 @@ public class PlayerInfoTile extends VBox {
         ageLabel.getStyleClass().add("player-age");
         super.getChildren().add(ageLabel);
 
+        overallGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .minValue(0).maxValue(100).decimals(0)
+                .minWidth(200).maxWidth(200)
+                .barColor(Color.BLUEVIOLET)
+                .title("Overall")
+                .build();
+        super.getChildren().add(overallGauge);
+
+        var statsRow = new HBox(10);
+        statsRow.setPadding(Insets.EMPTY);
+        statsRow.setAlignment(Pos.BASELINE_CENTER);
+        statsRow.getStyleClass().add("player-stats");
+        super.getChildren().add(statsRow);
+
+        battingGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .minValue(0).maxValue(100).decimals(0)
+                .minWidth(80).maxWidth(80)
+                .barColor(Color.CRIMSON)
+                .title("BAT")
+                .build();
+        statsRow.getChildren().add(battingGauge);
+
+        pitchingGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .minValue(0).maxValue(100).decimals(0)
+                .minWidth(80).maxWidth(80)
+                .barColor(Color.CRIMSON)
+                .title("PIT")
+                .build();
+        statsRow.getChildren().add(pitchingGauge);
+
+        fieldingGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .minValue(0).maxValue(100).decimals(0)
+                .minWidth(80).maxWidth(80)
+                .barColor(Color.CRIMSON)
+                .title("FLD")
+                .build();
+        statsRow.getChildren().add(fieldingGauge);
+
+        physicalGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .minValue(0).maxValue(100).decimals(0)
+                .minWidth(80).maxWidth(80)
+                .barColor(Color.CRIMSON)
+                .title("PHY")
+                .build();
+        statsRow.getChildren().add(physicalGauge);
+
     }
 
     public void reload(PlayerInfo player) {
         if (player != null) {
             nameLabel.setText(player.getName());
             ageLabel.setText(player.getAge() + " yrs");
+
+            overallGauge.setValue(player.getAttributes().getTotal());
+            battingGauge.setValue(player.getAttributes().getBattingSkill());
+            pitchingGauge.setValue(player.getAttributes().getPitchingSkill());
+            fieldingGauge.setValue(player.getAttributes().getFieldingSkill());
+            physicalGauge.setValue(player.getAttributes().getPhysicalSkill());
         }
     }
 }
