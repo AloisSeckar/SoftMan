c57cbf89261e438fe292416b36f96a4b5f6ee7db
Alois Sečkár <ellrohir@seznam.cz>
2022-05-14 10:35:36 +0200

    matches have status

--- src/main/java/elrh/softman/logic/Match.java
+++ src/main/java/elrh/softman/logic/Match.java
@@ -2,7 +2,9 @@ package elrh.softman.logic;
 
 import elrh.softman.logic.stats.*;
 import javafx.scene.control.TextArea;
+import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,20 +12,22 @@ import java.util.List;
 public class Match {
     @Getter
     private final LocalDate matchDay;
-
     @Getter
     private final Team awayTeam;
     @Getter
     private final Team homeTeam;
-
     @Getter
     private final BoxScore boxScore = new BoxScore();
+    @Getter
+    @Setter
+    private MatchStatus status;
     private List<Stats> stats;
 
     public Match(LocalDate matchDay, Team awayTeam, Team homeTeam) {
         this.matchDay = matchDay;
         this.awayTeam = awayTeam;
         this.homeTeam = homeTeam;
+        this.status = MatchStatus.SCHEDULED;
     }
 
     public void setStats(List<Stats> stats) {
@@ -35,6 +39,8 @@ public class Match {
         sim.simulateMatch();
         
         boxScore.printBoxScore(target);
+
+        this.status = MatchStatus.PLAYED;
     }
     
 }
---
+++ src/main/java/elrh/softman/logic/MatchStatus.java
@@ -0,0 +1,7 @@
+package elrh.softman.logic;
+
+public enum MatchStatus {
+    SCHEDULED,
+    IN_PROGRESS,
+    PLAYED
+}
