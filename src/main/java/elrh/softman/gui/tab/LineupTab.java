AloisSeckar/SoftMan

commit 89e8f515090c5ab16a259e81c19c2a13fc3819a4

data model rework - Lineup vol 3
Lineup object introduced into Match instead of Team instances + inherited changes.

Several pieces of code broken now. Flaws will be fixed ASAP in upcoming commits.

src/main/java/elrh/softman/gui/tab/LineupTab.java
@@ -6,13 +6,11 @@ import elrh.softman.logic.AssociationManager;
 import elrh.softman.logic.core.Club;
 import elrh.softman.logic.core.Team;
 import elrh.softman.logic.core.lineup.Lineup;
-import elrh.softman.logic.core.lineup.PlayerRecord;
 import elrh.softman.logic.interfaces.IFocusListener;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 public class LineupTab extends AnchorPane implements IFocusListener {
@@ -71,7 +69,7 @@ public class LineupTab extends AnchorPane implements IFocusListener {
             Lineup lineup = lineupTile.getLineup();
             AssociationManager.getInstance().getState().getActiveClub().getTeams().get(0).setLineup(lineup);
             for (int i = 1; i <= Lineup.POSITION_PLAYERS; i++) {
-                var current = lineup.getCurrentPositionPlayer(i);
+                var current = lineup.getCurrentBatter(i);
                 if (current != null) {
                     defenseTile.setPosition(current);
                 }

src/main/java/elrh/softman/gui/tab/MatchTab.java
@@ -94,8 +94,9 @@ public class MatchTab extends BorderPane {
     public void setMatch(Match match) {
         this.match = match;
         matchHeaderTile.setMatch(match);
-        awayLineup.fillLineup(match.getAwayTeam());
-        homeLineup.fillLineup(match.getHomeTeam());
+        // TODO temporary broken due to data model changes
+        //awayLineup.fillLineup(match.getAwayLineup());
+        //homeLineup.fillLineup(match.getHomeLineup());
         boxScore.loadBoxScore(match);
         match.printPlayByPlay(matchOverview);
 

src/main/java/elrh/softman/gui/tile/LineupTile.java
@@ -77,7 +77,7 @@ public class LineupTile extends VBox {
     }
 
     public Lineup getLineup() {
-        var ret = new Lineup(team.getId(), team.getName());
+        var ret = new Lineup(team.getId(), team.getName(), team.getLogo());
 
         lineupRows.stream().filter(LineupRowTile::isFilled).forEach(row -> ret.initPositionPlayer(row.getRow(), row.getCurrentSelection()));
 

src/main/java/elrh/softman/logic/core/lineup/Lineup.java
@@ -1,6 +1,7 @@
 package elrh.softman.logic.core.lineup;
 
 import elrh.softman.logic.Result;
+import elrh.softman.logic.enums.PlayerPosition;
 import elrh.softman.utils.Constants;
 import elrh.softman.utils.Utils;
 import java.util.*;
@@ -18,6 +19,8 @@ public class Lineup {
     private final long teamId;
     @Getter
     private final String teamName;
+    @Getter
+    private final String teamLogo;
 
     @Getter
     private final List<PlayerRecord>[] positionPlayers = new ArrayList[POSITION_PLAYERS];
@@ -25,9 +28,10 @@ public class Lineup {
     @Getter
     private final PlayerRecord[] substitutes = new PlayerRecord[SUBSTITUTES]; // TODO change type to PlayerInfo
 
-    public Lineup(long teamId, String teamName) {
+    public Lineup(long teamId, String teamName, String teamLogo) {
         this.teamId = teamId;
         this.teamName = teamName;
+        this.teamLogo = teamLogo;
 
         for (int i = 0; i < 9; i++) {
             positionPlayers[i] = new ArrayList<>();
@@ -68,14 +72,31 @@ public class Lineup {
         }
     }
 
-    public PlayerRecord getCurrentPositionPlayer(int batOrder) {
+    public PlayerRecord getCurrentBatter(int batOrder) {
         if (batOrder > 0 && batOrder <= POSITION_PLAYERS) {
             var lineupSpot = positionPlayers[batOrder - 1];
-            if (lineupSpot != null && lineupSpot.size() > 0) {
+            if (Utils.listNotEmpty(lineupSpot)) {
                 return lineupSpot.get(lineupSpot.size() - 1);
             }
         }
         return null;
+    }
+
+    public PlayerRecord getCurrentPositionPlayer(PlayerPosition position) {
+        PlayerRecord ret = null;
+        if (position != null) {
+            for (int i = 0; i < POSITION_PLAYERS; i++) {
+                var lineupSpot = positionPlayers[i];
+                if (Utils.listNotEmpty(lineupSpot)) {
+                    var player = lineupSpot.get(lineupSpot.size() - 1);
+                    if (position == player.getPosition()) {
+                        ret = player;
+                        break;
+                    }
+                }
+            }
+        }
+        return ret;
     }
 
     public boolean useDP() {

src/main/java/elrh/softman/gui/tile/BoxScoreTile.java
@@ -53,8 +53,8 @@ public class BoxScoreTile extends VBox {
     }
 
     public void loadBoxScore(Match match) {
-        awayTeam.setText(match.getAwayTeam().getName());
-        homeTeam.setText(match.getHomeTeam().getName());
+        awayTeam.setText(match.getAwayLineup().getTeamName());
+        homeTeam.setText(match.getHomeLineup().getTeamName());
         var boxScore = match.getBoxScore();
         if (boxScore != null) {
             innings.getChildren().clear();

src/main/java/elrh/softman/gui/tile/MatchHeaderTile.java
@@ -38,9 +38,9 @@ public class MatchHeaderTile extends BorderPane {
 
     public void setMatch(Match match) {
         if (match != null) {
-            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayTeam().getLogo())));
-            titleLabel.setText(match.getAwayTeam().getName() + " vs. " + match.getHomeTeam().getName());
-            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeTeam().getLogo())));
+            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayLineup().getTeamLogo())));
+            titleLabel.setText(match.getAwayLineup().getTeamName() + " vs. " + match.getHomeLineup().getTeamName());
+            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeLineup().getTeamLogo())));
         }
     }
 

src/main/java/elrh/softman/gui/tile/ScheduleRowTile.java
@@ -102,8 +102,8 @@ public class ScheduleRowTile extends BorderPane {
 
             sim = new MatchSimulator(match, MatchTab.getTarget());
 
-            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayTeam().getLogo())));
-            Tooltip.install(awayImage, new Tooltip(match.getAwayTeam().getName()));
+            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayLineup().getTeamLogo())));
+            Tooltip.install(awayImage, new Tooltip(match.getAwayLineup().getTeamName()));
 
             switch (match.getMatchInfo().getStatus()) {
                 case SCHEDULED -> titleLabel.setText(match.getMatchInfo().getMatchDay().toString() + " @ " + "Ballpark");
@@ -116,8 +116,8 @@ public class ScheduleRowTile extends BorderPane {
                 }
             }
 
-            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeTeam().getLogo())));
-            Tooltip.install(homeImage, new Tooltip(match.getHomeTeam().getName()));
+            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeLineup().getTeamLogo())));
+            Tooltip.install(homeImage, new Tooltip(match.getHomeLineup().getTeamName()));
 
         } else {
             sim = null;

src/main/java/elrh/softman/logic/AssociationManager.java
@@ -162,9 +162,10 @@ public class AssociationManager {
         for (var entry : getDailyMatches().entrySet()) {
             var leagueId = entry.getKey();
             entry.getValue().forEach(match -> {
-                if (state.userManagesTeam(match.getHomeTeam()) || state.userManagesTeam(match.getAwayTeam())) {
-                    ret.add(match);
-                }
+                // TODO temporary broken due to data model changes
+                //if (state.userManagesTeam(match.getHomeLineup()) || state.userManagesTeam(match.getAwayLineup())) {
+                //    ret.add(match);
+                //}
             });
         }
         return ret;

commit 1a0dfc628f4df3ab12c51c85fa1b3ad338985ca3

fixed 'fillLineup' method
was broken with 89e8f51

src/main/java/elrh/softman/gui/tab/LineupTab.java
@@ -33,7 +33,7 @@ public class LineupTab extends AnchorPane implements IFocusListener {
         playerTeam.randomizeLineup();
 
         lineupTile = new LineupTile(false);
-        lineupTile.fillLineup(playerTeam);
+        lineupTile.fillLineup(playerTeam.getDefaultLineup());
         super.getChildren().add(lineupTile);
         AnchorPane.setLeftAnchor(lineupTile, 10d);
         AnchorPane.setTopAnchor(lineupTile, 10d);

src/main/java/elrh/softman/gui/tab/MatchTab.java
@@ -94,9 +94,8 @@ public class MatchTab extends BorderPane {
     public void setMatch(Match match) {
         this.match = match;
         matchHeaderTile.setMatch(match);
-        // TODO temporary broken due to data model changes
-        //awayLineup.fillLineup(match.getAwayLineup());
-        //homeLineup.fillLineup(match.getHomeLineup());
+        awayLineup.fillLineup(match.getAwayLineup());
+        homeLineup.fillLineup(match.getHomeLineup());
         boxScore.loadBoxScore(match);
         match.printPlayByPlay(matchOverview);
 

src/main/java/elrh/softman/gui/tile/LineupTile.java
@@ -1,56 +1,66 @@
 package elrh.softman.gui.tile;
 
+import elrh.softman.logic.AssociationManager;
 import elrh.softman.logic.core.lineup.Lineup;
+import static elrh.softman.logic.core.lineup.Lineup.*;
+import elrh.softman.logic.db.GameDBManager;
 import elrh.softman.logic.db.orm.PlayerInfo;
-import elrh.softman.logic.core.Team;
 import elrh.softman.logic.core.lineup.PlayerRecord;
+import elrh.softman.logic.db.orm.TeamInfo;
 import elrh.softman.logic.enums.PlayerPosition;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
 
 public class LineupTile extends VBox {
 
-    private static final int MAX_PLAYERS = Lineup.POSITION_PLAYERS + Lineup.SUBSTITUTES;
+    private final LineupRowTile[] positionPlayersRows = new LineupRowTile[POSITION_PLAYERS];
+    private final LineupRowTile[] substitutesRows = new LineupRowTile[SUBSTITUTES];
 
-    private final List<LineupRowTile> lineupRows = new ArrayList<>(MAX_PLAYERS);
-
-    private Team team;
+    private TeamInfo team;
 
     public LineupTile(boolean readOnly) {
         super.setSpacing(5);
 
-        for (int i = 0; i < MAX_PLAYERS; i++) {
+        for (int i = 0; i < POSITION_PLAYERS; i++) {
             var lineupRowTile = new LineupRowTile(i);
-            lineupRows.add(lineupRowTile);
+            positionPlayersRows[i] = lineupRowTile;
             super.getChildren().add(lineupRowTile);
+        }
 
-            if (i == 8) {
-                super.getChildren().add(new Separator());
-            }
+        super.getChildren().add(new Separator());
+
+        for (int i = 0; i < SUBSTITUTES; i++) {
+            var lineupRowTile = new LineupRowTile(i);
+            substitutesRows[i] = lineupRowTile;
+            super.getChildren().add(lineupRowTile);
         }
 
         setReadOnly(readOnly);
     }
 
-    public void fillLineup(Team team) {
-        this.team = team;
-        List<PlayerInfo> players = team.getPlayers();
-        for (int i = 0; i < MAX_PLAYERS; i++) {
-            lineupRows.get(i).setUp(players, i < 9 ? team.getBatter(i + 1) : null);
+    public void fillLineup(Lineup lineup) {
+        this.team = GameDBManager.getInstance().getTeam(lineup.getTeamId());
+        // TODO get to player list more directly and correctly
+        var club = AssociationManager.getInstance().getClubById(team.getClubInfo().getClubId());
+        var players = club.getTeams().get(0).getPlayers();
+        // TODO get to player list more directly and correctly
+        for (int i = 0; i < POSITION_PLAYERS; i++) {
+            positionPlayersRows[i].setUp(players, i < 9 ? lineup.getCurrentBatter(i + 1) : null);
         }
     }
 
     public void setReadOnly(boolean readOnly) {
-        lineupRows.forEach(row -> row.setReadOnly(readOnly));
+        Arrays.stream(positionPlayersRows).forEach(row -> row.setReadOnly(readOnly));
+        Arrays.stream(substitutesRows).forEach(row -> row.setReadOnly(readOnly));
     }
 
     public String checkLineup() {
-        var checkedLineup = new ArrayList<PlayerRecord>(MAX_PLAYERS);
-        for (int i = 0; i < MAX_PLAYERS; i++) {
+        var checkedLineup = new ArrayList<PlayerRecord>(POSITION_PLAYERS);
+        for (int i = 0; i < POSITION_PLAYERS; i++) {
 
-            var currentSelection = lineupRows.get(i).getCurrentSelection();
+            var currentSelection = positionPlayersRows[i].getCurrentSelection();
 
             if (i < 9) {
                 if (currentSelection.getPlayer() == null || currentSelection.getPosition() == null) {
@@ -77,9 +87,10 @@ public class LineupTile extends VBox {
     }
 
     public Lineup getLineup() {
-        var ret = new Lineup(team.getId(), team.getName(), team.getLogo());
+        var ret = new Lineup(team.getTeamId(), team.getName(), team.getClubInfo().getLogo());
 
-        lineupRows.stream().filter(LineupRowTile::isFilled).forEach(row -> ret.initPositionPlayer(row.getRow(), row.getCurrentSelection()));
+        Arrays.stream(positionPlayersRows).filter(LineupRowTile::isFilled).forEach(row -> ret.initPositionPlayer(row.getRow(), row.getCurrentSelection()));
+        Arrays.stream(substitutesRows).filter(LineupRowTile::isFilled).forEach(row -> ret.initSubstitute(row.getRow(), row.getCurrentSelection()));
 
         return ret;
     }

src/main/java/elrh/softman/logic/core/lineup/Lineup.java
@@ -13,7 +13,7 @@ import lombok.*;
 public class Lineup {
 
     public static final int POSITION_PLAYERS = 10;
-    public static final int SUBSTITUTES = 7;
+    public static final int SUBSTITUTES = 8;
 
     @Getter
     private final long teamId;
@@ -33,7 +33,7 @@ public class Lineup {
         this.teamName = teamName;
         this.teamLogo = teamLogo;
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < POSITION_PLAYERS; i++) {
             positionPlayers[i] = new ArrayList<>();
         }
     }

src/main/java/elrh/softman/utils/mock/MockTeamFactory.java
@@ -13,7 +13,7 @@ public class MockTeamFactory {
 
         Random rand = new Random();
         List<Integer> usedNumbers = new ArrayList<>();
-        for (int i = 0; i < 17; i++) {
+        for (int i = 0; i < 20; i++) {
             int number;
             do {
                 number = rand.nextInt(99) + 1;
@@ -21,7 +21,7 @@ public class MockTeamFactory {
             usedNumbers.add(number);
             team.addPlayer(PlayerFactory.getRandomPlayerInfo(level.getGender(), getRandomBirth(level), number));
         }
-        
+
         team.randomizeLineup();
         
         return team;     