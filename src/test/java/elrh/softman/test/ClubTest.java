AloisSeckar/SoftMan

commit 89e8f515090c5ab16a259e81c19c2a13fc3819a4

data model rework - Lineup vol 3
Lineup object introduced into Match instead of Team instances + inherited changes.

Several pieces of code broken now. Flaws will be fixed ASAP in upcoming commits.

src/main/java/elrh/softman/logic/AssociationManager.java
@@ -162,9 +162,10 @@ public class AssociationManager {
         for (var entry : getDailyMatches().entrySet()) {
             var leagueId = entry.getKey();
             entry.getValue().forEach(match -> {
-                if (state.userManagesTeam(match.getHomeTeam()) || state.userManagesTeam(match.getAwayTeam())) {
-                    ret.add(match);
-                }
+                // TODO temporary broken due to data model changes
+                //if (state.userManagesTeam(match.getHomeLineup()) || state.userManagesTeam(match.getAwayLineup())) {
+                //    ret.add(match);
+                //}
             });
         }
         return ret;

src/main/java/elrh/softman/gui/tab/LineupTab.java
@@ -6,13 +6,11 @@ import elrh.softman.logic.AssociationManager;
 import elrh.softman.logic.core.Club;
 import elrh.softman.logic.core.Team;
 import elrh.softman.logic.core.lineup.Lineup;
-import elrh.softman.logic.core.lineup.PlayerRecord;
 import elrh.softman.logic.interfaces.IFocusListener;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 public class LineupTab extends AnchorPane implements IFocusListener {
@@ -71,7 +69,7 @@ public class LineupTab extends AnchorPane implements IFocusListener {
             Lineup lineup = lineupTile.getLineup();
             AssociationManager.getInstance().getState().getActiveClub().getTeams().get(0).setLineup(lineup);
             for (int i = 1; i <= Lineup.POSITION_PLAYERS; i++) {
-                var current = lineup.getCurrentPositionPlayer(i);
+                var current = lineup.getCurrentBatter(i);
                 if (current != null) {
                     defenseTile.setPosition(current);
                 }

src/main/java/elrh/softman/gui/tab/MatchTab.java
@@ -94,8 +94,9 @@ public class MatchTab extends BorderPane {
     public void setMatch(Match match) {
         this.match = match;
         matchHeaderTile.setMatch(match);
-        awayLineup.fillLineup(match.getAwayTeam());
-        homeLineup.fillLineup(match.getHomeTeam());
+        // TODO temporary broken due to data model changes
+        //awayLineup.fillLineup(match.getAwayLineup());
+        //homeLineup.fillLineup(match.getHomeLineup());
         boxScore.loadBoxScore(match);
         match.printPlayByPlay(matchOverview);
 

src/main/java/elrh/softman/gui/tile/BoxScoreTile.java
@@ -53,8 +53,8 @@ public class BoxScoreTile extends VBox {
     }
 
     public void loadBoxScore(Match match) {
-        awayTeam.setText(match.getAwayTeam().getName());
-        homeTeam.setText(match.getHomeTeam().getName());
+        awayTeam.setText(match.getAwayLineup().getTeamName());
+        homeTeam.setText(match.getHomeLineup().getTeamName());
         var boxScore = match.getBoxScore();
         if (boxScore != null) {
             innings.getChildren().clear();

src/main/java/elrh/softman/gui/tile/LineupTile.java
@@ -77,7 +77,7 @@ public class LineupTile extends VBox {
     }
 
     public Lineup getLineup() {
-        var ret = new Lineup(team.getId(), team.getName());
+        var ret = new Lineup(team.getId(), team.getName(), team.getLogo());
 
         lineupRows.stream().filter(LineupRowTile::isFilled).forEach(row -> ret.initPositionPlayer(row.getRow(), row.getCurrentSelection()));
 

src/main/java/elrh/softman/gui/tile/MatchHeaderTile.java
@@ -38,9 +38,9 @@ public class MatchHeaderTile extends BorderPane {
 
     public void setMatch(Match match) {
         if (match != null) {
-            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayTeam().getLogo())));
-            titleLabel.setText(match.getAwayTeam().getName() + " vs. " + match.getHomeTeam().getName());
-            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeTeam().getLogo())));
+            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayLineup().getTeamLogo())));
+            titleLabel.setText(match.getAwayLineup().getTeamName() + " vs. " + match.getHomeLineup().getTeamName());
+            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeLineup().getTeamLogo())));
         }
     }
 

src/main/java/elrh/softman/gui/tile/ScheduleRowTile.java
@@ -102,8 +102,8 @@ public class ScheduleRowTile extends BorderPane {
 
             sim = new MatchSimulator(match, MatchTab.getTarget());
 
-            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayTeam().getLogo())));
-            Tooltip.install(awayImage, new Tooltip(match.getAwayTeam().getName()));
+            awayImage.setImage(new Image(getClass().getResourceAsStream(match.getAwayLineup().getTeamLogo())));
+            Tooltip.install(awayImage, new Tooltip(match.getAwayLineup().getTeamName()));
 
             switch (match.getMatchInfo().getStatus()) {
                 case SCHEDULED -> titleLabel.setText(match.getMatchInfo().getMatchDay().toString() + " @ " + "Ballpark");
@@ -116,8 +116,8 @@ public class ScheduleRowTile extends BorderPane {
                 }
             }
 
-            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeTeam().getLogo())));
-            Tooltip.install(homeImage, new Tooltip(match.getHomeTeam().getName()));
+            homeImage.setImage(new Image(getClass().getResourceAsStream(match.getHomeLineup().getTeamLogo())));
+            Tooltip.install(homeImage, new Tooltip(match.getHomeLineup().getTeamName()));
 
         } else {
             sim = null;

src/main/java/elrh/softman/logic/MatchSimulator.java
@@ -1,13 +1,10 @@
 package elrh.softman.logic;
 
+import elrh.softman.logic.core.lineup.Lineup;
 import elrh.softman.logic.db.orm.MatchPlayByPlay;
-import elrh.softman.logic.db.orm.PlayerAttributes;
-import elrh.softman.logic.db.orm.PlayerInfo;
 import elrh.softman.logic.core.Match;
-import elrh.softman.logic.core.Team;
 import elrh.softman.logic.enums.MatchStatus;
 import elrh.softman.logic.enums.PlayerPosition;
-import elrh.softman.logic.core.lineup.PlayerRecord;
 import elrh.softman.logic.core.stats.BoxScore;
 import java.util.Random;
 
@@ -24,8 +21,8 @@ public class MatchSimulator {
     private final Match match;
 
     private final BoxScore boxScore;
-    private final Team awayTeam;
-    private final Team homeTeam;
+    private final Lineup awayLineup;
+    private final Lineup homeLineup;
     
     private boolean inningStart = true;
     private int inning = 1;
@@ -37,8 +34,8 @@ public class MatchSimulator {
     public MatchSimulator(Match match, TextArea target) {
         this.target = target;
         this.match = match;
-        awayTeam = match.getAwayTeam();
-        homeTeam = match.getHomeTeam();
+        awayLineup = match.getAwayLineup();
+        homeLineup = match.getHomeLineup();
         boxScore = match.getBoxScore();
     }
 
@@ -74,15 +71,15 @@ public class MatchSimulator {
                 inningStart = false;
             }
 
-            PlayerInfo pitcher = top ? homeTeam.getFielder(PlayerPosition.PITCHER) : awayTeam.getFielder(PlayerPosition.PITCHER);
-            PlayerAttributes pitcherAttr = pitcher.getAttributes();
+            var pitcher = top ? homeLineup.getCurrentPositionPlayer(PlayerPosition.PITCHER): awayLineup.getCurrentPositionPlayer(PlayerPosition.PITCHER);
+            var pitcherAttr = pitcher.getPlayer().getAttributes();
 
             appendText("PITCHER: " + pitcher + " (" + pitcherAttr.getPitchingSkill() + ")\n");
 
-            PlayerRecord batter = top ? awayTeam.getBatter(awayBatter) : homeTeam.getBatter(homeBatter);
+            var batter = top ? awayLineup.getCurrentBatter(awayBatter) : homeLineup.getCurrentBatter(homeBatter);
             if (batter != null) {
-                PlayerInfo batterInfo = batter.getPlayer();
-                PlayerAttributes batterAttr = batterInfo.getAttributes();
+                var batterInfo = batter.getPlayer();
+                var batterAttr = batterInfo.getAttributes();
 
                 appendText("BATTER: " + batterInfo + " (" + batterAttr.getBattingSkill() + ")\n");
 
@@ -99,10 +96,10 @@ public class MatchSimulator {
                     } else {
 
                         int randomLocation = random.nextInt(9);
-                        PlayerInfo fielder = top ? homeTeam.getFielder(randomLocation) : awayTeam.getFielder(randomLocation);
+                        var fielder = top ? homeLineup.getCurrentBatter(randomLocation) : awayLineup.getCurrentBatter(randomLocation);
 
                         if (fielder != null) {
-                            int fieldingQuality = fielder.getAttributes().getFieldingSkill() + random.nextInt(100);
+                            int fieldingQuality = fielder.getPlayer().getAttributes().getFieldingSkill() + random.nextInt(100);
                             if (hitQuality >= fieldingQuality) {
                                 if (random.nextBoolean()) {
                                     appendText(batter + " reached after a hit\n");
@@ -142,7 +139,7 @@ public class MatchSimulator {
 
             if (outs == 3) {
                 getScore();
-                swapTeams();
+                swapLineups();
             }
 
             if (!keepPlaying()) {
@@ -155,7 +152,7 @@ public class MatchSimulator {
     ///////
     private void setUpMatch() {
         match.getMatchInfo().setStatus(MatchStatus.ACTIVE);
-        appendText("\n\nGAME BETWEEN " + awayTeam.getName() + " AND " + homeTeam.getName() + "\n");
+        appendText("\n\nGAME BETWEEN " + awayLineup.getTeamName() + " AND " + homeLineup.getTeamName() + "\n");
     }
 
     private void wrapUpMatch() {
@@ -176,7 +173,7 @@ public class MatchSimulator {
 
         if (top) {
             // match can end only before next inning
-            // after that away team will always finnish their batting
+            // after that away Lineup will always finnish their batting
             if (inningStart) {
                 if (inning >= 4 && (Math.abs(awayPoints - homePoints) >= 15)) {
                     ret = false;
@@ -195,7 +192,7 @@ public class MatchSimulator {
                 }
             }
         } else {
-            // match can end anytime hometeam gets in the lead or triggers mercy rule
+            // match can end anytime homeLineup gets in the lead or triggers mercy rule
             if (inning >= 3 && homePoints - awayPoints >= 15) {
                 ret = false;
             } else if (inning >= 4 && homePoints - awayPoints >= 10) {
@@ -209,7 +206,7 @@ public class MatchSimulator {
 
         return ret;
     }
-    private void swapTeams() {
+    private void swapLineups() {
         outs = 0;
         top = !top;
         if (top) {
@@ -219,8 +216,8 @@ public class MatchSimulator {
     }
 
     private void getScore() {
-        appendText("\n\n" + awayTeam.getName() + ": " + boxScore.getTotalPoints(true) + "\n");
-        appendText(homeTeam.getName() + ": " + boxScore.getTotalPoints(false) + "\n");
+        appendText("\n\n" + awayLineup.getTeamName() + ": " + boxScore.getTotalPoints(true) + "\n");
+        appendText(homeLineup.getTeamName() + ": " + boxScore.getTotalPoints(false) + "\n");
     }
     
     private void appendText(String text) {

commit fc335013c4a989f71b3d838f3fa5e686fd854b3b

fixed 'userManagesTeam' method
was broken with 89e8f51

src/main/java/elrh/softman/logic/AssociationManager.java
@@ -160,12 +160,10 @@ public class AssociationManager {
     public List<Match> getDailyMatchesForUser() {
         var ret = new ArrayList<Match>();
         for (var entry : getDailyMatches().entrySet()) {
-            var leagueId = entry.getKey();
             entry.getValue().forEach(match -> {
-                // TODO temporary broken due to data model changes
-                //if (state.userManagesTeam(match.getHomeLineup()) || state.userManagesTeam(match.getAwayLineup())) {
-                //    ret.add(match);
-                //}
+                if (state.userManagesTeam(match.getHomeLineup().getTeamId()) || state.userManagesTeam(match.getAwayLineup().getTeamId())) {
+                    ret.add(match);
+                }
             });
         }
         return ret;

src/main/java/elrh/softman/logic/core/Club.java
@@ -48,6 +48,9 @@ public class Club implements IDatabaseEntity {
     public List<Team> getTeams() {
         return teams.values().stream().toList();
     }
+    public List<Long> getTeamIds() {
+        return teams.values().stream().map(Team::getId).toList();
+    }
 
     public boolean isActive() {
         return getClubInfo().getRegistered() == AssociationManager.getInstance().getClock().getYear();

src/main/java/elrh/softman/logic/managers/StateManager.java
@@ -41,9 +41,9 @@ public class StateManager {
         focusedTeam = null;
     }
 
-    public boolean userManagesTeam(Team team) {
+    public boolean userManagesTeam(long teamId) {
         if (activeClub != null) {
-            return activeClub.getTeams().contains(team);
+            return activeClub.getTeamIds().contains(teamId);
         } else {
             return false;
         }

src/test/java/elrh/softman/test/ClubTest.java
@@ -52,4 +52,17 @@ public class ClubTest extends AbstractDBTest {
         assertTrue(teams.get(1).getTeamInfo().getName().endsWith("B"), "2nd team's name should end with 'B'");
         assertTrue(teams.get(2).getTeamInfo().getName().contains("U18"), "3rd team's name should contain 'U18'");
     }
+
+    @Test
+    @DisplayName("getTeamsTest")
+    void getTeamsTest() {
+        club.formTeam(PlayerLevel.MSEN);
+        club.formTeam(PlayerLevel.MSEN);
+
+        assertEquals(2, club.getTeams().size(), "there should be 2 teams in club");
+
+        var teamIds = club.getTeamIds();
+        assertTrue(teamIds.contains(2L), "there should be team with ID 2");
+        assertFalse(teamIds.contains(3L), "there shouldn't be team with ID 3");
+    }
 }