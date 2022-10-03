5c1f8f0270d6c0a41aa7231e5b99f8e9703e3f87
Alois Sečkár <ellrohir@seznam.cz>
2022-10-03 19:54:19 +0200

    expand stats overview

--- src/main/java/elrh/softman/gui/tab/PlayerTab.java
+++ src/main/java/elrh/softman/gui/tab/PlayerTab.java
@@ -37,6 +37,8 @@ public class PlayerTab extends BorderPane implements IFocusedTeamListener {
         selectPlayerCB.setMaxWidth(220d);
         selectPlayerCB.valueProperty().addListener((ov, oldValue, newValue) -> reload(newValue));
 
+        statsOverview.setStyle("-fx-font: 12px 'Courier New';");
+
         var attributesButton = new Button("Attributes");
         attributesButton.setAlignment(Pos.CENTER);
         var seasonStatsButton = new Button("Season stats");
@@ -56,20 +58,33 @@ public class PlayerTab extends BorderPane implements IFocusedTeamListener {
         playerInfo.reload(info);
 
         statsOverview.clear();
-        statsOverview.appendText("PLAYER                         | PA | AB |  H |  R | RBI |   AVG |  O |  IP | \n");
+        statsOverview.appendText("PLAYER                         |  PA |  AB |   R |   H |  2B |  3B |  HR |  SH |  SF |  BB |  HP |  SB |  CS |   K | RBI |   AVG |   SLG |  PO |   A |   E |    IP | \n");
         var player = AssociationManager.getInstance().getPlayerById(info.getPlayerId());
         if (player != null) {
             player.getStats().forEach(record -> {
                 var nameWithPos = record.getMatchId() + " " + record.getPlayerStr();
                 statsOverview.appendText(StringUtils.rightPad(nameWithPos, 30, " ") + " | ");
-                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBPA()), 2) + " | ");
-                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBAB()), 2) + " | ");
-                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBH()), 2) + " | ");
-                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBR()), 2) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBPA()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBAB()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBR()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBH()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getB2B()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getB3B()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBHR()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBSH()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBSF()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBBB()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBHP()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBSB()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBCS()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBK()), 3) + " | ");
                 statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getBRB()), 3) + " | ");
                 statsOverview.appendText(StatsUtils.getAVG(record.getBAB(), record.getBH()) + " | ");
-                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getFPO()), 2) + " | ");
-                statsOverview.appendText(StatsUtils.getIP(record.getFIP()) + " | \n");
+                statsOverview.appendText(StatsUtils.getSLG(record.getBAB(), record.getBH(), record.getB2B(), record.getB3B(), record.getBHR()) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getFPO()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getFA()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(String.valueOf(record.getFE()), 3) + " | ");
+                statsOverview.appendText(StringUtils.leftPad(StatsUtils.getIP(record.getFIP()), 5) + " | \n");
             });
         }
     }
