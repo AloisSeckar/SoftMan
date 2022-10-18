02e59adc3eec76835d1976ab25aa0457c3f809c5
Alois Sečkár <ellrohir@seznam.cz>
2022-10-18 22:37:50 +0200

    links to match from stats
    change from TextArea to TextFlow and redrawn code with JavaFX Hyperlink element

--- src/main/java/elrh/softman/gui/tab/PlayerTab.java
+++ src/main/java/elrh/softman/gui/tab/PlayerTab.java
@@ -7,17 +7,20 @@ import elrh.softman.logic.core.Team;
 import elrh.softman.logic.db.orm.player.PlayerInfo;
 import elrh.softman.logic.db.orm.player.PlayerStats;
 import elrh.softman.logic.interfaces.IFocusedTeamListener;
+import elrh.softman.utils.FormatUtils;
 import elrh.softman.utils.StatsUtils;
+import java.util.ArrayList;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.apache.commons.lang3.StringUtils;
-
-import java.util.ArrayList;
 
 public class PlayerTab extends BorderPane implements IFocusedTeamListener {
 
@@ -25,8 +28,8 @@ public class PlayerTab extends BorderPane implements IFocusedTeamListener {
     private final ObservableList<PlayerInfo> data;
     private final PlayerInfoTile playerInfo = new PlayerInfoTile(false);
     private final PlayerAttributesTile playerAttributesTA = new PlayerAttributesTile();
-    private final TextArea seasonStatsTA = new TextArea();
-    private final TextArea careerStatsTA = new TextArea();
+    private final TextFlow seasonStatsTA = new TextFlow();
+    private final TextFlow careerStatsTA = new TextFlow();
 
     private static PlayerTab INSTANCE;
     public static PlayerTab getInstance() {
@@ -46,8 +49,9 @@ public class PlayerTab extends BorderPane implements IFocusedTeamListener {
         selectPlayerCB.valueProperty().addListener((ov, oldValue, newValue) -> reload(newValue));
 
         seasonStatsTA.getStyleClass().add("font-courier-12");
+        seasonStatsTA.setPadding(FormatUtils.PADDING_5);
         careerStatsTA.getStyleClass().add("font-courier-12");
-        careerStatsTA.setText("Career stats");
+        careerStatsTA.setPadding(FormatUtils.PADDING_5);
 
         var attributesButton = new Button("Attributes");
         attributesButton.setAlignment(Pos.CENTER);
@@ -79,17 +83,21 @@ public class PlayerTab extends BorderPane implements IFocusedTeamListener {
 
             playerAttributesTA.reload(info.getAttributes());
 
-            seasonStatsTA.clear();
-            seasonStatsTA.appendText("MATCH           |  PA |  AB |   R |   H |  2B |  3B |  HR |  SH |  SF |  BB |  HP |  SB |  CS |   K | RBI |   AVG |   SLG |  PO |   A |   E |    IP | \n");
+            seasonStatsTA.getChildren().clear();
+            var sHeader = new Text("MATCH           |  PA |  AB |   R |   H |  2B |  3B |  HR |  SH |  SF |  BB |  HP |  SB |  CS |   K | RBI |   AVG |   SLG |  PO |   A |   E |    IP | \n");
+            seasonStatsTA.getChildren().add(sHeader);
             var player = AssociationManager.getInstance().getPlayerById(info.getPlayerId());
             if (player != null) {
                 player.getStats().forEach(record -> renderStatsRecord(seasonStatsTA, record, false, null));
+                renderStatsRecord(seasonStatsTA, player.getSeasonTotal(), false, null);
             }
-            renderStatsRecord(seasonStatsTA, player.getSeasonTotal(), false, null);
 
-            careerStatsTA.clear();
-            careerStatsTA.appendText("SEASON |   G |  PA |  AB |   R |   H |  2B |  3B |  HR |  SH |  SF |  BB |  HP |  SB |  CS |   K | RBI |   AVG |   SLG |  PO |   A |   E |    IP | \n");
-            renderStatsRecord(careerStatsTA, player.getSeasonTotal(), true, player.getStats().size());
+            careerStatsTA.getChildren().clear();
+            var cHeader = new Text("SEASON |   G |  PA |  AB |   R |   H |  2B |  3B |  HR |  SH |  SF |  BB |  HP |  SB |  CS |   K | RBI |   AVG |   SLG |  PO |   A |   E |    IP | \n");
+            careerStatsTA.getChildren().add(cHeader);
+            if (player != null) {
+                renderStatsRecord(careerStatsTA, player.getSeasonTotal(), true, player.getStats().size());
+            }
             // TODO make it variable for each year yet to come + make total career count
         }
     }
@@ -106,34 +114,51 @@ public class PlayerTab extends BorderPane implements IFocusedTeamListener {
         selectPlayerCB.setValue(selectPlayerCB.getItems().get(0));
     }
 
-    private void renderStatsRecord(TextArea target, PlayerStats record, boolean careerStats, Integer totalGames) {
+    private void renderStatsRecord(TextFlow target, PlayerStats record, boolean careerStats, Integer totalGames) {
         if (careerStats) {
             int year = AssociationManager.getInstance().getClock().getYear();
-            target.appendText(StringUtils.rightPad(String.valueOf(year), 6, " ") + " | ");
-            target.appendText(StringUtils.leftPad(String.valueOf(totalGames), 3) + " | ");
+            target.getChildren().add(new Text(StringUtils.rightPad(String.valueOf(year), 6, " ") + " | "));
+            target.getChildren().add(new Text(StringUtils.leftPad(String.valueOf(totalGames), 3) + " | "));
         } else {
-            target.appendText(StringUtils.rightPad(record.getMatchStr(), 15, " ") + " | ");
+            if ("Season total".equals(record.getMatchStr())) {
+                target.getChildren().add(new Text(StringUtils.rightPad(record.getMatchStr(), 15, " ") + " | "));
+            } else {
+                var matchLink = new Hyperlink(StringUtils.rightPad(record.getMatchStr(), 15, " "));
+                matchLink.setBorder(Border.EMPTY);
+                matchLink.setPadding(new Insets(4, 0, 4, 0));
+                matchLink.setOnAction(e -> {
+                    var alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("You requested match " + record.getMatchId());
+                    alert.showAndWait();
+                });
+                target.getChildren().add(matchLink);
+                target.getChildren().add(new Text(" | "));
+            }
         }
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBPA()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBAB()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBR()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBH()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getB2B()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getB3B()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBHR()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBSH()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBSF()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBBB()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBHP()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBSB()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBCS()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBK()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getBRB()), 3) + " | ");
-        target.appendText(StatsUtils.getAVG(record.getBAB(), record.getBH()) + " | ");
-        target.appendText(StatsUtils.getSLG(record.getBAB(), record.getBH(), record.getB2B(), record.getB3B(), record.getBHR()) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getFPO()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getFA()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(String.valueOf(record.getFE()), 3) + " | ");
-        target.appendText(StringUtils.leftPad(StatsUtils.getIP(record.getFIP()), 5) + " | \n");
+
+        var sb = new StringBuilder();
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBPA()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBAB()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBR()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBH()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getB2B()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getB3B()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBHR()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBSH()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBSF()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBBB()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBHP()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBSB()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBCS()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBK()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getBRB()), 3)).append(" | ");
+        sb.append(StatsUtils.getAVG(record.getBAB(), record.getBH())).append(" | ");
+        sb.append(StatsUtils.getSLG(record.getBAB(), record.getBH(), record.getB2B(), record.getB3B(), record.getBHR())).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getFPO()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getFA()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(String.valueOf(record.getFE()), 3)).append(" | ");
+        sb.append(StringUtils.leftPad(StatsUtils.getIP(record.getFIP()), 5)).append(" | \n");
+        target.getChildren().add(new Text(sb.toString()));
+
     }
 }
